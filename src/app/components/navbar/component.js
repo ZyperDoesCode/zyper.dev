a22d25061244babd319510abbcef2feeb9d90232
Zyper <nithishpravin010@gmail.com>
2023-10-28 23:05:53 +0530

    [feat]: Nav bar added (Unresponsive as of now)

--- src/app/components/navbar/component.js
+++ src/app/components/navbar/component.js
@@ -1,12 +1,39 @@
+import { GrMail } from "react-icons/gr";
+
 export default function Navbar() {
-    return (
-        <nav className="h-[60px] relative bg-white m-10 items-center justify-center flex">
-            <div className="flex flex-row">
-                <div>
-                    <h1>Zyper</h1>
-                </div>
-            </div>
-        </nav>
-    )
-  }
-  
+  return (
+    <nav className="h-[90px] w-[100%] relative m-10 items-center flex flex-row place-content-between p-10">
+      <div className="flex flex-row m-10 items-center justify-center space-x-12">
+        <div>
+          <h1 className="font-outfit text-4xl text-white">Zyper</h1>
+        </div>
+        <div>
+          <p className="font-inconsolata text-xl text-white mt-4 ml-6 cursor-pointer underline">
+            <span className="text-[#7a85ff] font-bold">/</span>home
+          </p>
+        </div>
+        <div>
+          <p className="font-inconsolata text-xl text-white mt-4 cursor-pointer underline">
+            <span className="text-[#7a85ff] font-bold">/</span>stack
+          </p>
+        </div>
+        <div>
+          <p className="font-inconsolata text-xl text-white mt-4 cursor-pointer underline">
+            <span className="text-[#7a85ff] font-bold">/</span>portfolio
+          </p>
+        </div>
+        <div>
+          <p className="font-inconsolata text-xl text-white mt-4 cursor-pointer underline">
+            <span className="text-[#7a85ff] font-bold">/</span>donate
+          </p>
+        </div>
+      </div>
+      <div>
+        <div className="m-10 h-10 w-40 bg-[#616dff] flex rounded text-white items-center justify-center cursor-pointer font-outfit hover:scale-105 ease-in duration-200 underline">
+          <GrMail className="mr-2" />
+          Contact Me
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/app/pages/home/home.js
+++ src/app/pages/home/home.js
@@ -2,7 +2,7 @@ import Navbar from "../../components/navbar/component"
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center bg-gradient-to-r from-[#282565] to-[#04020a]">
+        <main className="flex min-h-screen flex-col items-center">
             <Navbar/>
         </main>
     )
